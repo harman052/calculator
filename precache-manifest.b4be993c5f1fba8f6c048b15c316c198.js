b13d26e131195f04b5e85b5290b4421484519c4b
Harmanpreet Singh <harman052@gmail.com>
2018-12-01 20:11:10 +0530

    Updates

---
+++ precache-manifest.b4be993c5f1fba8f6c048b15c316c198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa636ee70adc70200f3",
+    "url": "/photoShoto/static/css/main.d04ccad4.chunk.css"
+  },
+  {
+    "revision": "2aa636ee70adc70200f3",
+    "url": "/photoShoto/static/js/main.2aa636ee.chunk.js"
+  },
+  {
+    "revision": "3e69e9c02f0827542073",
+    "url": "/photoShoto/static/css/1.fcb88278.chunk.css"
+  },
+  {
+    "revision": "3e69e9c02f0827542073",
+    "url": "/photoShoto/static/js/1.3e69e9c0.chunk.js"
+  },
+  {
+    "revision": "86bfe0df8992fa560a8e",
+    "url": "/photoShoto/static/js/runtime~main.86bfe0df.js"
+  },
+  {
+    "revision": "2b0ab89fb6230f7acdf6b803617f43b3",
+    "url": "/photoShoto/index.html"
+  }
+];
